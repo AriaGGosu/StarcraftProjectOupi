6d7374a3a5cf8b2ff9971666536577dc84beb710
AriaGGosu <hector.vicencio.reyes@gmail.com>
2019-04-02 13:10:49 -0300

    i make changes in the footer

---
+++ hector.js
@@ -0,0 +1,30 @@
+
+/* const numberOne = 10;
+const numberTwo = 20;
+
+console.log('la suma es : ' , numberOne + numberTwo);
+
+
+*/
+
+const suma = (num1 , num2) => {
+    total = num1 + num2;
+    return console.log(total);
+}
+
+
+suma(10,20);
+suma('hola','hector');
+suma(100,200);
+
+var suma = function(n1,n2){
+    total = n1 + n2;
+    return console.log(total);
+}
+
+const numero1 = 20;
+const numero2 = 10;
+
+suma(10,20);
+
+/* este codigo es solo para aprender de java scrip*/
